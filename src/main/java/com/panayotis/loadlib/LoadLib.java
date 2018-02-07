733f36257479e3b0d44a80cffe04a85a201d5513
Panayotis Katsaloulis <panayotis@panayotis.com>
2018-02-07 15:55:11 +0200

    auto-multi-lib support

--- src/main/java/com/panayotis/loadlib/LoadLib.java
+++ src/main/java/com/panayotis/loadlib/LoadLib.java
@@ -5,43 +5,84 @@
  */
 package com.panayotis.loadlib;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLEncoder;
 
 /**
- *
  * @author teras
  */
 public class LoadLib {
 
-    @SuppressWarnings("UseSpecificCatch")
     public static boolean load(String resourcepath) {
-        byte[] buffer = new byte[0x4000];
+        return load(resourcepath, (File) null);
+    }
+
+    public static boolean load(Class signatureClass, String resourcepath) {
+        if (resourcepath == null || signatureClass == null)
+            return load(resourcepath, (File) null);
+        File outFile;
+        try {
+            URL location = signatureClass.getProtectionDomain().getCodeSource().getLocation();
+            outFile = new File(System.getProperty("user.home") + File.separator
+                    + ".cache" + File.separator + "loadlib"
+                    + File.separator + "libs" + File.separator
+                    + URLEncoder.encode(location.toString(), "UTF-8")
+                    + File.separator + geFileName(resourcepath));
+            File origFile = getFileFromURL(location);
+            if (origFile != null && outFile.isFile() && origFile.lastModified() > outFile.lastModified())
+                outFile.delete();
+        } catch (Throwable e) {
+            outFile = null;
+        }
+        return load(resourcepath, outFile);
+    }
+
+    private static boolean load(String resourcepath, File destLib) {
+        if (resourcepath == null)
+            return false;
+        File fileOut = destLib == null ? tempLibLoc(geFileName(resourcepath)) : destLib;
+        if (fileOut == null)
+            return false;
+        fileOut.getParentFile().mkdirs();
+        if (!fileOut.isFile())
+            dumpLib(fileOut, resourcepath);
+        System.load(fileOut.getAbsolutePath());
+//        if (destLib == null)
+//            fileOut.deleteOnExit();
+        return true;
+    }
+
+
+    private static File tempLibLoc(String name) {
+        File temp;
+        try {
+            temp = File.createTempFile("embed", "lib");
+        } catch (IOException e) {
+            return null;
+        }
+        temp.delete();
+        return new File(temp, name);
+    }
+
+    private static String geFileName(String resourcepath) {
+        return resourcepath.substring(resourcepath.lastIndexOf('/') + 1, resourcepath.length());
+    }
+
+    private static boolean dumpLib(File fileOut, String resourcepath) {
         BufferedInputStream in = null;
         BufferedOutputStream out = null;
         try {
-            String name = resourcepath.substring(resourcepath.lastIndexOf('/') + 1, resourcepath.length());
-            File temp = File.createTempFile("embed", "lib");
-            temp.delete();
-            File fout = new File(temp, name);
-            fout.getParentFile().mkdirs();
-
-            out = new BufferedOutputStream(new FileOutputStream(fout), buffer.length);
+            byte[] buffer = new byte[0x16000];
+            out = new BufferedOutputStream(new FileOutputStream(fileOut), buffer.length);
             in = new BufferedInputStream(LoadLib.class.getResourceAsStream(resourcepath), buffer.length);
             int size;
             while ((size = in.read(buffer)) >= 0)
                 out.write(buffer, 0, size);
-            in.close();
             out.flush();
-            out.close();
-
-            System.load(fout.getAbsolutePath());
-            fout.deleteOnExit();
             return true;
-        } catch (Throwable ex) {
+        } catch (IOException ex) {
             return false;
         } finally {
             if (in != null)
@@ -55,5 +96,29 @@ public class LoadLib {
                 } catch (IOException ex) {
                 }
         }
+    }
+
+    private static File getFileFromURL(URL url) {
+        File f;
+        try {
+            f = new File(url.toURI());
+        } catch (URISyntaxException e) {
+            f = new File(url.getPath());
+        }
+        return f.isFile() ? f : null;
+    }
+
+    public static String getLibName(String path, String basename) {
+        String bits = System.getProperty("os.arch", "").contains("64") ? "64" : "32";
+        String os = System.getProperty("os.name", "").toLowerCase();
+        String prefix = os.contains("windows") ? "" : "lib";
+        String suffix = os.contains("windows") ? ".dll" : (os.contains("mac") ? ".dylib" : ".so");
+        return (path.startsWith("/") ? "" : "/")
+                + path
+                + (path.endsWith("/") ? "" : "/")
+                + prefix
+                + basename
+                +bits
+                + suffix;
     }
 }
